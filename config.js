d960cb5619bf9ece2f411abacec0414b4254d4db
Marszl <Marszl@users.noreply.github.com>
2016-03-31 00:09:06 +0200

    Update config.js
    Database url aangepast

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
 
 //	"database": "mongodb://<dbuser>:<dbpassword>@ds013569.mlab.com:13569/userstory",
-	"database": "mongodb://root:abc123@ds013569.mlab.com:13569/userstory",
-
+//	"database": "mongodb://root:abc123@ds013569.mlab.com:13569/userstory",
+	"database": "mongodb://heroku_5znf0lpj:6f3qbfuh39us9dn06s7bb4ags6@ds011810.mlab.com:11810/heroku_5znf0lpj",
 	"port": process.env.PORT || 5000,
 	"secretKey": "Bazinga"
 }
