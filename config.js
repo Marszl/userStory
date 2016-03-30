7782e19253e5f9791f09127111f52ee54ef321bb
Marszl <Marszl@users.noreply.github.com>
2016-03-30 23:34:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
 //	"database": "mongodb://<dbuser>:<dbpassword>@ds013569.mlab.com:13569/userstory",
 	"database": "mongodb://root:abc123@ds013569.mlab.com:13569/userstory",
 
-	"port": process.env.PORT || 3000,
+	"port": process.env.PORT || 5000,
 	"secretKey": "Bazinga"
 }
